26898a1a6b76a591b05f4f1a3bad7f084d25dba4
Moises Correa <145796144+MoicoeMT@users.noreply.github.com>
2024-01-26 11:54:43 +0000

    firstdatamodel

--- src/models.py
+++ src/models.py
@@ -7,23 +7,66 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key = True)
+    email = Column(String(50), nullable = False )
+    password = Column(String(500), nullable = False )
+    profile = relationship("profile")
+
+class Profile(Base):
+    __tablename__ = "profile"
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=True)
+    last_name = Column(String(250), nullable=True)
+    phone_number = Column(Integer, nullable=True)
+    city = Column(String(250), nullable=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class People(Base):
+    __tablename__ = "people"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=True)
+    last_name = Column(String(250), nullable=True)
+    birth_year = Column(String(250), nullable=True)
+    eye_color = Column(String(250), nullable=True)
+    hair_color = Column(String(250), nullable=True)
+    height = Column(Integer, nullable=True)
+    favorit = relationship("favorit")
+
+class Planet(Base):
+    __tablename__ = "planet"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+    climate = Column(String(250), nullable=True)
+    diameter = Column(String(250), nullable=True)
+    gravity = Column(Integer, nullable=True)
+    population = Column(Integer, nullable=True)
+    terrain = Column(String(250), nullable=True)
+    favorit = relationship("favorit")
+
+class Vehicle(Base):
+    __tablename__ = "vehicle"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+    consumables = Column(String(250), nullable=True)
+    manufacturer = Column(String(250), nullable=True)
+    model = Column(String(250), nullable=True)
+    passengers = Column(Integer, nullable=True)
+    terrain = Column(String(250), nullable=True)
+    cargo_capacity = Column(Integer, nullable=True)
+    cost_in_credits = Column(Integer, nullable=True)
+    favorit = relationship("favorit")
+
+class Favorit(Base):
+    __tablename__ = "favorit"
+    id = Column(Integer, primary_key=True)
+    people_id = Column(Integer, ForeignKey("people.id"))
+    planet_id = Column(Integer, ForeignKey("planet.id"))
+    vehicle_id = Column(Integer, ForeignKey("vehicle.id"))
+
 
     def to_dict(self):
         return {}
